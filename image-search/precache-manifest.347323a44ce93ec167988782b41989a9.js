0aa5be2d39ec9be2ddaa61cb22e775e4a27043d9
rajjubajra <rajjubajra@gmail.com>
2022-06-08 06:05:30 +0100

    image-search added

---
+++ image-search/precache-manifest.347323a44ce93ec167988782b41989a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02811139a949e8d2ece7",
+    "url": "/image-search/static/css/main.f4096b89.chunk.css"
+  },
+  {
+    "revision": "02811139a949e8d2ece7",
+    "url": "/image-search/static/js/main.02811139.chunk.js"
+  },
+  {
+    "revision": "8925d08cca978cb70ab7",
+    "url": "/image-search/static/js/1.8925d08c.chunk.js"
+  },
+  {
+    "revision": "430cf7d360eea82a327d",
+    "url": "/image-search/static/js/runtime~main.430cf7d3.js"
+  },
+  {
+    "revision": "20ff1b2594ba9851c4d6cec7813bd548",
+    "url": "/image-search/index.html"
+  }
+];
